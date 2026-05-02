2334dc078101a4dae18b91b13a1faca28d3f42b2
QASIM-ARAIN <qasim.shakeel35@gmail.com>
2026-05-02 21:53:00 +0500

    Added square and square root operation

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import math
 
 def calculate(operation):
     try:
@@ -22,10 +23,27 @@ def calculate(operation):
     except ValueError:
         label_result.config(text="Invalid input! Enter numbers only.", fg="red")
 
+def calculate_single(operation):
+    try:
+        num1 = float(entry_num1.get())
+
+        if operation == "sq":
+            result = num1 ** 2
+        elif operation == "sqrt":
+            if num1 < 0:
+                label_result.config(text="Cannot find square root of negative!", fg="red")
+                return
+            result = math.sqrt(num1)
+
+        label_result.config(text=f"Result: {result}", fg="green")
+
+    except ValueError:
+        label_result.config(text="Invalid input! Enter numbers only.", fg="red")
+
 
 window = tk.Tk()
 window.title("GUI Calculator")
-window.geometry("350x300")
+window.geometry("350x350")
 window.config(bg="#1e1e1e")
 
 
@@ -38,22 +56,27 @@ tk.Label(window, text="Enter Second Number:", bg="#1e1e1e", fg="white").pack(pad
 entry_num2 = tk.Entry(window, width=30, justify="center")
 entry_num2.pack()
 
-# Operation buttons
-frame = tk.Frame(window, bg="#1e1e1e")
-frame.pack(pady=10)
 
-tk.Button(frame, text="+", width=8, height=2, bg="#D17C69", fg="white",
+frame1 = tk.Frame(window, bg="#1e1e1e")
+frame1.pack(pady=10)
+
+tk.Button(frame1, text="Add (+)", width=8, height=2, bg="#D17C69", fg="white",
           command=lambda: calculate("+")).pack(side="left", padx=5)
-
-tk.Button(frame, text="-", width=8, height=2, bg="#D17C69", fg="white",
+tk.Button(frame1, text="Sub (-)", width=8, height=2, bg="#D17C69", fg="white",
           command=lambda: calculate("-")).pack(side="left", padx=5)
-
-tk.Button(frame, text="*", width=8, height=2, bg="#D17C69", fg="white",
+tk.Button(frame1, text="Mul (*)", width=8, height=2, bg="#D17C69", fg="white",
           command=lambda: calculate("*")).pack(side="left", padx=5)
-
-tk.Button(frame, text="/", width=8, height=2, bg="#D17C69", fg="white",
+tk.Button(frame1, text="Div (/)", width=8, height=2, bg="#D17C69", fg="white",
           command=lambda: calculate("/")).pack(side="left", padx=5)
 
+
+frame2 = tk.Frame(window, bg="#1e1e1e")
+frame2.pack()
+
+tk.Button(frame2, text="Square (x²)", width=12, height=2, bg="#3a86ff", fg="white",
+          command=lambda: calculate_single("sq")).pack(side="left", padx=5)
+tk.Button(frame2, text="Sqrt (√x)", width=12, height=2, bg="#3a86ff", fg="white",
+          command=lambda: calculate_single("sqrt")).pack(side="left", padx=5)
 
 label_result = tk.Label(window, text="Result: ", font=("Arial", 14), bg="#1e1e1e", fg="white")
 label_result.pack(pady=10)
